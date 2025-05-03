34c7bd2d3c969cdd57594728e649990f6ffaa3a8
aradmehralian <arad.mehralian@gmail.com>
2025-05-03 17:34:35 +0330

    Implement RandomPasswordGenerator, PinCodeGenerator, Add utils directory, constants and pools modules

---
+++ Level I/Password Generator/utils/constants.py
@@ -0,0 +1,5 @@
+import string
+
+_DIGITS = string.digits
+_LETTERS = string.ascii_letters
+_SYMBOLS = string.punctuation
---
+++ Level I/Password Generator/utils/pools.py
@@ -0,0 +1,15 @@
+from .constants import _DIGITS, _LETTERS, _SYMBOLS
+
+
+def build_char_pool(include_digits=False, include_symbols=False) -> str:
+    """
+    Create character pool to indicate which characters to be in the passwords.
+    """
+    pool = _LETTERS
+
+    if include_digits:
+        pool += _DIGITS
+    if include_symbols:
+        pool += _SYMBOLS
+
+    return pool
