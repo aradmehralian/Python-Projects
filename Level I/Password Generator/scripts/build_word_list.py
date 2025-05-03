a96e4264f7abe4f4659d6e8165a4f0841ba6f073
aradmehralian <arad.mehralian@gmail.com>
2025-05-03 22:35:06 +0330

    Add script to download NLTK's words corpus process and save it in data/sample words.txt

---
+++ Level I/Password Generator/scripts/build_word_list.py
@@ -0,0 +1,22 @@
+import json
+import nltk
+from nltk.corpus import words
+
+
+def filter_words():
+    """
+    Get the words in the NLTK's words corpus and filter based on length.
+    Save the filtered list to `data/sample words.txt`
+    """
+
+    nltk.download("words")
+    words_list = words.words()
+
+    filtered_words = [word for word in words_list if 4 <= len(word) <= 8]
+
+    with open("data/sample words.txt", mode="w") as f:
+        json.dump(filtered_words, f)
+
+
+if __name__ == "__main__":
+    filter_words()
