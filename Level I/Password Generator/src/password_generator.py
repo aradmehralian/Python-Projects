4b849756a565b1f9ee827bbe3f621a8dd18dd62e
aradmehralian <arad.mehralian@gmail.com>
2025-05-04 09:35:53 +0330

    Use secrets module instead of random

--- Level I/Password Generator/src/password_generator.py
+++ Level I/Password Generator/src/password_generator.py
@@ -1,30 +1,30 @@
 from abc import ABC, abstractmethod
 import json
-import random
+from typing import List
+import secrets
 
 
 from utils.characters import _DIGITS
 from utils.pools import build_char_pool
 
 
-
 __all__ = ["PinCodeGenerator", "RandomPasswordGenerator", "MemorablePasswordGenerator"]
 
 
-with open("data/sample words.txt") as f:
-    filtered_words = json.load(f)
+def load_vocabulary() -> List[str]:
+    """
+    Load and return a list of sample words from `data/`
+    """
+
+    with open("data/sample words.txt") as f:
+        return json.load(f)
 
 
 class PasswordGenerator(ABC):
-    def __init__(self, length: int):
-        self.length = self._validate_length(length)
 
-    def _validate_length(self, length: int) -> int:
-        if (not isinstance(length, int)) or (length < 0):
-            raise ValueError("Password length must be a non-negative integer")
-        if 8 <= length <= 25:
-            return length
-        raise ValueError("Password length must be between 8 and 25 inclusive")
+    @abstractmethod
+    def _validate_length(self):
+        pass
 
     @abstractmethod
     def generate(self):
@@ -33,7 +33,7 @@ class PasswordGenerator(ABC):
 
 class PinCodeGenerator(PasswordGenerator):
     def __init__(self, length: int = 4):
-        super().__init__(length)
+        self.length = self._validate_length(length)
 
     def _validate_length(self, length: int) -> int:
         if (not isinstance(length, int)) or (length < 0):
@@ -43,8 +43,7 @@ class PinCodeGenerator(PasswordGenerator):
         raise ValueError("Pin code length must be between 4 and 12 inclusive")
 
     def generate(self) -> str:
-        random_pin = random.choices(_DIGITS, k=self.length)
-        return "".join(random_pin)
+        return "".join(secrets.choice(_DIGITS) for _ in range(self.length))
 
     def __call__(self) -> str:
         return self.generate()
@@ -52,7 +51,14 @@ class PinCodeGenerator(PasswordGenerator):
 
 class RandomPasswordGenerator(PasswordGenerator):
     def __init__(self, length: int = 12):
-        super().__init__(length)
+        self.length = self._validate_length(length)
+
+    def _validate_length(self, length: int) -> int:
+        if (not isinstance(length, int)) or (length < 0):
+            raise ValueError("Password length must be a non-negative integer")
+        if 8 <= length <= 25:
+            return length
+        raise ValueError("Password length must be between 8 and 25 inclusive")
 
     def generate(
         self, include_digits: bool = False, include_symbols: bool = False
@@ -60,18 +66,34 @@ class RandomPasswordGenerator(PasswordGenerator):
 
         pool = build_char_pool(include_digits, include_symbols)
 
-        chars = random.choices(pool, k=self.length)
-        return "".join(chars)
+        return "".join(secrets.choice(pool) for _ in range(self.length))
 
 
 class MemorablePasswordGenerator(PasswordGenerator):
-    def __init__(self, num_words: int = 5):
-        self.num_words = num_words
+    def __init__(self, num_words: int = 5, vocabulary: List[str] = None):
+        self.num_words = self._validate_length(num_words)
+
+        if vocabulary is None:
+            self.vocabulary = load_vocabulary()
+        else:
+            self.vocabulary = self._validate_vocab_size(num_words, vocabulary)
 
     def generate(self, sep: str = "-", capitalized: bool = False) -> str:
-        random_words = random.choices(filtered_words, k=self.num_words)
+        random_words = [secrets.choice(self.vocabulary) for _ in range(self.num_words)]
 
         if capitalized:
             return sep.join(word.capitalize() for word in random_words)  # generator exp
 
         return sep.join(random_words)
+
+    def _validate_vocab_size(self, num_words: int, vocabulary: List[str]) -> int:
+        if len(vocabulary) >= num_words:
+            return vocabulary
+        raise ValueError("Not enough words in the vocabulary")
+
+    def _validate_length(self, num_words: int) -> int:
+        if (not isinstance(num_words, int)) or (num_words < 0):
+            raise ValueError(
+                "Number of words must be a non-negative integer"
+            )
+        return num_words
