7c3db4f5a213298a82522149167d3f46bdc39a56
aradmehralian <arad.mehralian@gmail.com>
2025-05-05 10:36:02 +0330

    Add documentation

--- Level I/Password Generator/src/password_generator.py
+++ Level I/Password Generator/src/password_generator.py
@@ -11,21 +11,47 @@ __all__ = ["PinCodeGenerator", "RandomPasswordGenerator", "MemorablePasswordGene
 
 
 class PasswordGenerator(ABC):
+    """
+    Base Class for all the password generators.
+    """
 
     @abstractmethod
     def _validate_length(self):
+        """
+        Child classes must implement this method.
+        """
         pass
 
     @abstractmethod
     def generate(self):
+        """
+        Child classes must implement this method.
+        """
         pass
 
 
 class PinCodeGenerator(PasswordGenerator):
+    """
+    Class to generate random pin codes.
+    """
+
     def __init__(self, length: int = 4):
         self.length = self._validate_length(length)
 
     def _validate_length(self, length: int) -> int:
+        """
+        Validate the length of pin code.
+
+        Args:
+            length (int): The length provided by the user
+        
+        Returns:
+            length (int): The validated length.
+        
+        Raises:
+            ValueError: If input length is not a valid number or if input length is not in the limit range.
+        """
+
         if (not isinstance(length, int)) or (length < 0):
             raise ValueError("Pin code length must be a non-negative integer")
         if 4 <= length <= 12:
@@ -33,6 +59,10 @@ class PinCodeGenerator(PasswordGenerator):
         raise ValueError("Pin code length must be between 4 and 12 inclusive")
 
     def generate(self) -> str:
+        """
+        Generate numeric pin code.
+        """
+
         return "".join(secrets.choice(_DIGITS) for _ in range(self.length))
 
     def __call__(self) -> str:
@@ -40,6 +70,10 @@ class PinCodeGenerator(PasswordGenerator):
 
 
 class RandomPasswordGenerator(PasswordGenerator):
+    """
+    Class to generate random passwords.
+    """
+
     def __init__(
         self,
         length: int = 12,
@@ -50,6 +84,19 @@ class RandomPasswordGenerator(PasswordGenerator):
         self.pool = build_char_pool(include_digits, include_symbols)
 
     def _validate_length(self, length: int) -> int:
+        """
+        Validate the length of password.
+
+        Args:
+            length (int): The length provided by the user
+        
+        Returns:
+            length (int): The validated length.
+        
+        Raises:
+            ValueError: If input length is not a valid number or if input length is not in the limit range.
+        """
+
         if (not isinstance(length, int)) or (length < 0):
             raise ValueError("Password length must be a non-negative integer")
         if 8 <= length <= 25:
@@ -57,6 +104,10 @@ class RandomPasswordGenerator(PasswordGenerator):
         raise ValueError("Password length must be between 8 and 25 inclusive")
 
     def generate(self) -> str:
+        """
+        Generate a random password with the given criteria from the characters in `self.pool`
+        """
+
         return "".join(secrets.choice(self.pool) for _ in range(self.length))
 
     def __call__(self):
@@ -64,6 +115,10 @@ class RandomPasswordGenerator(PasswordGenerator):
 
 
 class MemorablePasswordGenerator(PasswordGenerator):
+    """
+    Class to generate memorable passwords.
+    """
+
     def __init__(
         self,
         num_words: int = 5,
@@ -82,6 +137,19 @@ class MemorablePasswordGenerator(PasswordGenerator):
         self.vocabulary = vocabulary
 
     def _validate_length(self, num_words: int) -> int:
+        """"
+        Validate the number of words in the password.
+
+        Args:
+            num_words (int): The number of words specified by the user.
+
+        Returns:
+            num_words (int): The validated number of words.
+
+        Raises:
+            ValueError: If the provided `num_words` is not valid or exceeds the maximum limit.
+        """
+
         if (not isinstance(num_words, int)) or (num_words < 0):
             raise ValueError("Number of words must be a non-negative integer")
         if 0 < num_words <= 8:
@@ -89,11 +157,29 @@ class MemorablePasswordGenerator(PasswordGenerator):
         raise ValueError("Number of words exceeded maximum limit, 8")
 
     def _validate_vocab_size(self, num_words: int, vocabulary: List[str]) -> int:
+        """
+        Validate the size of the user-provided vocabulary.
+
+        Args:
+            num_words (int): The number of words in the password.
+            vocabulary (List[str]): The vocabulary to choose the words from.
+        
+        Returns:
+            vocabulary (List[str]): The validated vocabulary.
+        
+        Raises:
+            ValueError: If the number of words in the provided vocabulary is less than the number of words in the password.
+        """
+
         if len(vocabulary) >= num_words:
             return vocabulary
         raise ValueError("Not enough words in the vocabulary")
 
     def generate(self) -> str:
+        """
+        Generate a memorable password with the given criteria from `self.vocabulary`
+        """
+
         random_words = [secrets.choice(self.vocabulary) for _ in range(self.num_words)]
 
         if self.capitalized:
--- Level I/Password Generator/utils/pools.py
+++ Level I/Password Generator/utils/pools.py
@@ -1,10 +1,18 @@
 from .characters import _DIGITS, _LETTERS, _SYMBOLS
 
 
-def build_char_pool(include_digits=False, include_symbols=False) -> str:
+def build_char_pool(include_digits: bool = False, include_symbols: bool = False) -> str:
     """
-    Create character pool to indicate which characters to be in the passwords.
+    Create a character pool to draw from for the password.
+
+    Args:
+        include_digits (bool): Include digits in the character pool.
+        include_symbols (bool): Include symbols in the character pool.
+
+    Returns:
+        pool (str): The built character pool based on the given criteria.
     """
+
     pool = _LETTERS
 
     if include_digits:
